f7ec56ec4cb63c5f2cf6f69521a7c95b7a2029df
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-04 18:25:18 -0300

    feat: create input component

---
+++ src/app/components/Input/Input.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface InputProps {
+  type: string;
+  placeholder: string;
+}
+
+export default function Input({ type, placeholder }: InputProps) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className='p-2 rounded bg-zinc-800 text-white outline-none transition-colors duration-300 ease-in-out focus:bg-gray-600'
+    />
+  )
+}
