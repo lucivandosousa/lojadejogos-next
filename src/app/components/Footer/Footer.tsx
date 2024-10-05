756801c00a7112acec457d5172abf9781e36ab75
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-04 22:17:15 -0300

    fix: add link style

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-zinc-900 text-white py-8 px-4 text-center text-md">
       <p>Todos os direitos reservados &copy; 2024</p>
       <p>
-        <a href="https://github.com/lucivandosousa" target="_blank" className="text-amber-500">Visite meu portfólio</a> | <a href="https://www.linkedin.com/in/lucivandosousa" target="_blank">Conecte-se comigo no LinkedIn</a>
+        <a href="https://github.com/lucivandosousa" target="_blank" className="text-amber-500">Visite meu portfólio</a> | <a href="https://www.linkedin.com/in/lucivandosousa" target="_blank" className="text-amber-500">Conecte-se comigo no LinkedIn</a>
       </p>
     </footer>
   )
